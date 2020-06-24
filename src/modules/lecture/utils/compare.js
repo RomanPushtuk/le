308c09664aa5d429b58b56c6e9270421a9eefc82
Roman Pushtuk <r123321456@gmail.com>
2020-06-24 22:33:17 +0300

    1231312

--- src/modules/lecture/utils/compare.js
+++ src/modules/lecture/utils/compare.js
@@ -1,17 +1,12 @@
 export function compare(answer, correctAnswer) {
     const mapCorrectAnswers = {};
-    const mapUserAnswers = {};
 
     correctAnswer.forEach((answer) => {
         mapCorrectAnswers[answer] = answer;
     });
 
-    answer.forEach((answer) => {
-        mapUserAnswers[answer] = answer;
-    });
-
-    for (const answer in mapCorrectAnswers) {
-        if (mapCorrectAnswers[answer] !== mapUserAnswers[answer]) return false;
+    for (const value of answer) {
+        if (mapCorrectAnswers[value] !== value) return false;
     }
 
     return true;
